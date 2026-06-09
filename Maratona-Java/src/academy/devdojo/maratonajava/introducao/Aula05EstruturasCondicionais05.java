b5cbe8fe9516dc08f52fdcfdb763ba046b1cd6b2
Gabriel Arthur <l3rieldev@gmail.com>
2026-06-09 00:46:41 -0300

    feat: add new Java course files and update existing exercises

---
+++ Maratona-Java/src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais05.java
@@ -0,0 +1,48 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais05 {
+    public static void main(String[] args) {
+        byte dia = 5;
+        switch (dia) {
+            case 1:
+                System.out.println("domingo");
+                break;
+            case 2:
+                System.out.println("segunda");
+                break;
+            case 3:
+                System.out.println("terça");
+                break;
+            case 4:
+                System.out.println("quarta");
+                break;
+            case 5:
+                System.out.println("quinta");
+                break;
+            case 6:
+                System.out.println("sexta");
+                break;
+            case 7:
+                System.out.println("sabado");
+                break;
+            default:
+                System.out.println("opção inválida");
+
+        }
+
+        char sexo = 'M';
+        switch (sexo) {
+            case 'M':
+                System.out.println("Homem");
+                break;
+            case 'F':
+                System.out.println("Mulher");
+                break;
+            default:
+                System.out.println("Inválido");
+                break;
+
+
+        }
+    }
+}
