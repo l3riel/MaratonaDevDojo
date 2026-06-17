a8d32562adf0f9dabc94829eb755a465276bba92
Gabriel Arthur <l3rieldev@gmail.com>
2026-06-17 01:12:35 -0300

    feat: add exemples on arrays structures and multidimensional arrays

---
+++ Maratona-Java/src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula08ArraysMultidimensionais01 {
+    public static void main(String[] args) {
+        // 1,2,3,4,5,6,7,8,9,10,12 meses
+        //31,28,31,30 dias
+        int[][] dias = new  int[2][2];
+
+    }
+}
